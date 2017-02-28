683396d3cf7267e6c261a89f1c85453e99e0a9a4
Jaka <jakaspeh@gmail.com>
2017-02-28 06:35:27 +0100

    removing some unused code...

--- jdesigner/io.py
+++ jdesigner/io.py
@@ -7,7 +7,6 @@ from .bezier_curve import BezierCurve
 
 
 def split_strings(file_name, split="*"):
-
     strings = []
     s = ""
     with open(file_name) as file:
@@ -28,60 +27,6 @@ def split_strings(file_name, split="*"):
     return strings
 
 
-def string_to_curve(s):
-    lines = s.split("\n")
-
-    if lines[0] != "Bezier curve:":
-        raise ValueError("String does not represent a Bezier curve")
-
-    degree = int(lines[2])
-
-    control_points = []
-    for i in range(degree + 1):
-        line = lines[4 + i]
-        words = line.split(" ")
-        x = float(words[0])
-        y = float(words[1])
-
-        control_points.append([x, y])
-
-    return BezierCurve(control_points)
-
-
-def read_curves(file_name):
-    curves = []
-    with open(file_name) as stream:
-
-        s = ""
-
-        for line in stream:
-
-            if line == "\n":
-                if s != "":
-                    curve = string_to_curve(s)
-                    curves.append(curve)
-                    s = ""
-            else:
-                s += line
-    return curves
-
-
-def curve_to_string(curve):
-    control_points = curve.get_control_points()
-    degree = len(control_points) - 1
-
-    s = ""
-    s += "Bezier curve:\n"
-    s += "Degree:\n" + str(degree) + "\n"
-    s += "Control points:\n"
-
-    for point in control_points:
-        x, y = point
-        s += str(x) + " " + str(y) + "\n"
-
-    return s
-
-
 def get_open_filename():
     file_name = QtGui.QFileDialog.getOpenFileName(None,
                                                   "Load File",
@@ -93,18 +38,15 @@ def get_open_filename():
 
     return file_name
 
+
 def construct_object(string, viewbox):
-    object = None
+    obj = None
     if "*JRectangle" in string:
-        object = Jrectangle.load(string, info_dock=viewbox.info_dock,
-                                 viewbox=viewbox)
+        obj = Jrectangle.load(string, info_dock=viewbox.info_dock, viewbox=viewbox)
     if "*JBezierCurve" in string:
-        object = BezierCurve.load(string, info_dock=viewbox.info_dock,
-                                  viewbox=viewbox)
+        obj = BezierCurve.load(string, info_dock=viewbox.info_dock, viewbox=viewbox)
     if "*JPolyline" in string:
-        object = JpolyLine.load(string, info_dock=viewbox.info_dock,
-                                viewbox=viewbox)
+        obj = JpolyLine.load(string, info_dock=viewbox.info_dock, viewbox=viewbox)
     if "*JText" in string:
-        object = JtextROI.load(string, info_dock=viewbox.info_dock,
-                               viewbox=viewbox)
-    return object
+        obj = JtextROI.load(string, info_dock=viewbox.info_dock, viewbox=viewbox)
+    return obj

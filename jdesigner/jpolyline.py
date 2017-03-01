e3f67669a657ca014d998d51e38c61699e59ec20
Jaka <jakaspeh@gmail.com>
2017-03-01 06:28:18 +0100

    deleting a print statement...

--- jdesigner/jpolyline.py
+++ jdesigner/jpolyline.py
@@ -52,7 +52,6 @@ class JpolyLine(ROI, JChooseColor, JArrowDock, JRemoveItem):
             print("Error the string is in the wrong format.")
 
         data = eval(s)
-        print("Laoding", data["control points"])
         polyline = cls(data["control points"], info_dock=info_dock,
                        viewbox=viewbox, arrow=data["arrow"],
                        arrow_start=data["arrow start"],
